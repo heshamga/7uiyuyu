bcba9163aa947fab0bde9e3af5835401d42affd6
heshamga <52211963+heshamga@users.noreply.github.com>
2019-06-27 03:07:32 -1000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,22 @@
+﻿ const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("593190562010628104")
+setInterval(function() {
+channel.send(`dfytrtrtyrtyrytrtyrtyrtyrtyrytrtyrytrytrytry`);
+}, 30)
+})
+
+
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "!ratep") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+client.login(process.env.BOT_TOKEN); 
